1be6c6abc6282ed86bb540e8127cc384713faf39
Rtransat <florent.sorel@gmail.com>
2022-11-22 19:34:10 +0100

    feat: add mysql connection pool

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,14 +1,19 @@
 package main
 
 import (
+	"context"
+	"database/sql"
 	_ "embed"
 	"flag"
 	"fmt"
 	"log"
 	"net/http"
 	"os"
+	"time"
 
 	"github.com/rtransat/go-api-example/internal/jsonlog"
+
+	_ "github.com/go-sql-driver/mysql"
 )
 
 //go:embed version.txt
@@ -17,6 +22,12 @@ var version string
 type config struct {
 	port int
 	env  string
+	db   struct {
+		dsn             string
+		maxLifetimeConn string
+		maxOpenConns    int
+		maxIdleConns    int
+	}
 }
 
 type application struct {
@@ -29,9 +40,25 @@ func main() {
 	flag.IntVar(&cfg.port, "port", 4000, "API server port")
 	flag.StringVar(&cfg.env, "env", "development", "Environment (development|stating|production)")
 
+	dsn := os.Getenv("KINOW_API_DB_DSN")
+	if len(dsn) == 0 {
+		dsn = "root:toor@/golang_api?parseTime=true"
+	}
+
+	flag.StringVar(&cfg.db.dsn, "db-dsn", dsn, "MySQL DSN")
+	flag.StringVar(&cfg.db.maxLifetimeConn, "db-max-lifetime-conn", "3m", "MySQL max lifetime connection")
+	flag.IntVar(&cfg.db.maxOpenConns, "db-max-open-conns", 10, "MySQL max open connections")
+	flag.IntVar(&cfg.db.maxIdleConns, "db-max-idle-conns", 10, "MySQL max idle connections")
+
 	flag.Parse()
 
 	logger := jsonlog.New(os.Stdout, jsonlog.LevelInfo)
+
+	db, err := openDB(cfg)
+	if err != nil {
+		logger.PrintFatal(err, nil)
+	}
+	defer db.Close()
 
 	app := &application{
 		config: cfg,
@@ -49,8 +76,34 @@ func main() {
 		"env":  cfg.env,
 	})
 
-	err := srv.ListenAndServe()
+	err = srv.ListenAndServe()
 	if err != nil {
 		logger.PrintFatal(err, nil)
 	}
+}
+
+func openDB(cfg config) (*sql.DB, error) {
+	db, err := sql.Open("mysql", cfg.db.dsn)
+	if err != nil {
+		return nil, err
+	}
+
+	duration, err := time.ParseDuration(cfg.db.maxLifetimeConn)
+	if err != nil {
+		return nil, err
+	}
+	db.SetConnMaxLifetime(duration)
+
+	db.SetMaxOpenConns(cfg.db.maxOpenConns)
+	db.SetMaxIdleConns(cfg.db.maxIdleConns)
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	err = db.PingContext(ctx)
+	if err != nil {
+		return nil, err
+	}
+
+	return db, nil
 }
--- go.mod
+++ go.mod
@@ -3,3 +3,5 @@ module github.com/rtransat/go-api-example
 go 1.19
 
 require github.com/go-chi/chi v1.5.4
+
+require github.com/go-sql-driver/mysql v1.6.0
